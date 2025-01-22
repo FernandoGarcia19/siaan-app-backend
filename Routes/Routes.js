4d11c095ecc1736b7f68c02e9faf47f234757712
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2025-01-21 22:04:45 -0400

    Add Router

---
+++ Routes/Routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const AppRouter = express.Router();
+const ClassController = require('../Controllers/Class.controller');
+const HorarioController = require('../Controllers/Horario.controller');
+
+AppRouter.get('/Classes/', ClassController.getClasses());
+AppRouter.get('/Horarios/', HorarioController.getHorarios());
+
+module.exports = AppRouter;
